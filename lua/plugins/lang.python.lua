55aca8ce9c0db1253428427abeab917ca5b0ba9d
Chad Phillips <chad@apartmentlines.com>
2025-02-24 09:44:54 -0500

    tweak basedpyright config to discover venv and disable some reporting

--- lua/plugins/lang.python.lua
+++ lua/plugins/lang.python.lua
@@ -1,3 +1,14 @@
+local util = require("util")
+
+local function set_up_python_config()
+  local python = {}
+  local venv_name = util.get_active_pyenv_env()
+  if venv_name then
+    python.pythonPath = string.format("/home/hunmonk/.pyenv/versions/%s/bin/python", venv_name)
+  end
+  return python
+end
+
 return {
   {
     "neovim/nvim-lspconfig",
@@ -5,17 +16,24 @@ return {
       servers = {
         basedpyright = {
           settings = {
-            python = {
-                analysis = {
-                    diagnosticSeverityOverrides = {
-                        reportGeneralTypeIssues = "warning",
-                    },
-                    typeCheckingMode = "off",
-                },
-            },
             basedpyright = {
-                disableOrganizeImports = true,
+              analysis = {
+                diagnosticSeverityOverrides = {
+                  reportGeneralTypeIssues = "warning",
+                  reportAny = "none",
+                  reportExplicitAny = "none",
+                  reportMissingTypeStubs = "none",
+                  reportUnknownArgumentType = "none",
+                  reportUnknownVariableType = "none",
+                  reportUnknownMemberType = "none",
+                  reportUnknownParameterType = "none",
+                  reportUnusedCallResult = "none",
+                },
+              },
+              disableOrganizeImports = true,
+              disableTaggedHints = true,
             },
+            python = set_up_python_config(),
           },
         },
       },
