bea8905912c4ddf0f6b2417f7c798579721e89cd
Chad Phillips <chad@apartmentlines.com>
2025-03-03 10:46:53 -0500

    remove unneeded lazygit config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -26,20 +26,3 @@ local codeium_ignore_filetypes = {
 }
 vim.g.codeium_filetypes = codeium_ignore_filetypes
 vim.g.codeium_no_map_tab = true
-vim.g.lazygit_config = {
-  theme = {
-    lightTheme = false,
-    activeBorderColor = { "#89b4fa", "bold" },
-    inactiveBorderColor = { "#a6adc8" },
-    optionsTextColor = { "#89b4fa" },
-    selectedLineBgColor = { "#313244" },
-    cherryPickedCommitBgColor = { "#45475a" },
-    cherryPickedCommitFgColor = { "#89b4fa" },
-    unstagedChangesColor = { "#f38ba8" },
-    defaultFgColor = { "#cdd6f4" },
-    searchingActiveBorderColor = { "#f9e2af" },
-  },
-  authorColors = {
-    ['*'] = '#b4befe',
-  }
-}
